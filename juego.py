2debb4914aee691b16d023d270b73b3f46fae857
Danellys Oriana Rondon Peña <danellysoriana22@gmail.com>
2024-11-10 13:32:20 -0300

    Eliminar escapar_isla.py y modificar juego.py

--- juego.py
+++ juego.py
@@ -2,6 +2,7 @@ from ingresar import *
 from archivos_funciones import agregar_jugador
 import random
 
+
 def evento_aleatorio(eventos: dict) -> int:
     """
     Imprime evento aleatorio
@@ -16,7 +17,7 @@ def evento_aleatorio(eventos: dict) -> int:
     evento = random.choice(list(eventos.values()))
     print(evento["mensaje"])
     estado = 0
-
+    resultado = 0
     while estado == 0:
 
         print("""
@@ -24,7 +25,7 @@ def evento_aleatorio(eventos: dict) -> int:
         2-No hacer accion!
               """)
 
-        opcion = ingresar_entero("Ingrese una opcion: ")
+        opcion = ingresar_entero("Que camino desea  elegir (1/2): ")
 
         if opcion == 1:
             resultado = evento["opcion_1"]
@@ -33,8 +34,8 @@ def evento_aleatorio(eventos: dict) -> int:
         elif opcion == 2:
             resultado = evento["opcion_2"]
             estado = 1
-
     return resultado
+
 
 def lista_eventos() -> dict:
     """
@@ -49,36 +50,37 @@ def lista_eventos() -> dict:
 
     eventos = {
 
-        "evento_1": {"mensaje": "Encontro una estatua lo investigara o se ira? ", 
-                     "opcion_1": 50, 
+        "evento_1": {"mensaje": "Encontro una estatua lo investigara o se ira? ",
+                     "opcion_1": 50,
                      "opcion_2": 25},
 
-        "evento_2": {"mensaje": "Encontro un campamento lo investigara o se ira? ", 
-                     "opcion_1": 25, 
+        "evento_2": {"mensaje": "Encontro un campamento lo investigara o se ira? ",
+                     "opcion_1": 25,
                      "opcion_2": 25},
 
-        "evento_3": {"mensaje": "Encontro una escritura rara lo analizara mas cerca o no lo hara? ", 
-                     "opcion_1": 45, 
+        "evento_3": {"mensaje": "Encontro una escritura rara lo analizara mas cerca o no lo hara? ",
+                     "opcion_1": 45,
                      "opcion_2": 25},
 
-        "evento_4": {"mensaje": "Encontro una vibora atacara o huira? ", 
-                     "opcion_1": 25, 
+        "evento_4": {"mensaje": "Encontro una vibora atacara o huira? ",
+                     "opcion_1": 25,
                      "opcion_2": -25},
 
-        "evento_5": {"mensaje": "Encontro una losa de piedra lo recogera o lo dejara en el suelo? ", 
-                     "opcion_1": 50, 
+        "evento_5": {"mensaje": "Encontro una losa de piedra lo recogera o lo dejara en el suelo? ",
+                     "opcion_1": 50,
                      "opcion_2": 25},
 
-        "evento_6": {"mensaje": "Encontro un ??? atacara o huira? ", 
-                     "opcion_1": -75, 
+        "evento_6": {"mensaje": "Encontro un ??? atacara o huira? ",
+                     "opcion_1": -75,
                      "opcion_2": 25},
 
-        "evento_7": {"mensaje": "Encontro un animal no identificado atacara o huira? ", 
-                     "opcion_1": 10, 
+        "evento_7": {"mensaje": "Encontro un animal no identificado atacara o huira? ",
+                     "opcion_1": 10,
                      "opcion_2": 10}
     }
 
     return eventos
+
 
 def obtener_puntos(sobrevivir, jugador) -> dict:
     """
@@ -94,12 +96,14 @@ def obtener_puntos(sobrevivir, jugador) -> dict:
     if sobrevivir == 20:
         jugador["puntos"] = jugador["puntos"] * sobrevivir
         print("Logro sobrevivir y escapo con exito vino un barco de casualidad y logro escapar!!")
-        print(f"Puntos finales del jugador {jugador['nombre']}: {jugador['puntos']}")
+        print(f"Puntos finales del jugador {
+              jugador['nombre']}: {jugador['puntos']}")
 
     else:
         jugador["puntos"] = jugador["puntos"] * sobrevivir
-        print("Perdio, gracias por jugar esta partida!!")
-        print(f"Puntos finales del jugador {jugador['nombre']}: {jugador['puntos']}")
+        print("\nPerdio, gracias por jugar esta partida!!")
+        print(f"\nPuntos finales del jugador {
+              jugador['nombre']}: {jugador['puntos']}")
 
     return jugador
 
@@ -115,28 +119,31 @@ def comenzar_juego() -> None:
     None
     """
 
-    nombre = ingresar_nombre("Ingrese como se llama su jugador: ")
-    jugador = {"nombre": nombre, "salud": 100, "puntos": 0}
+    nombre = ingresar_nombre("\n¿Cuál es tu nombre, aventurero?: ")
+    jugador = {"nombre": nombre,
+               "salud": 100,
+               "puntos": 0,
+               }
     lista = []
     sobrevivir = 0
     estado = 0
-    
+
     eventos = lista_eventos()
 
     while estado == 0:
 
         resultado = evento_aleatorio(eventos)
 
-        print(f"el resultado {resultado}")
+        print(f"\nResultado {resultado}")
 
         if resultado > 0:
             jugador["puntos"] += resultado
-            print(f"Obtuvo {resultado} puntos! ")
-            print(f"Sus puntos: {jugador['puntos']}.")
+            print(f"\nFelicidades Ganaste {resultado} puntos! ")
+            print(f"Tus puntos: {jugador['puntos']}.")
 
         else:
             jugador["salud"] += resultado
-            print(f"le hicieron {resultado} de herida cuidado! ")
+            print(f"\nLe hicieron {resultado} de herida cuidado! ")
             print(f"Su salud actual: {jugador['salud']}.")
 
         if sobrevivir == 20 or jugador["salud"] <= 0:
@@ -144,10 +151,10 @@ def comenzar_juego() -> None:
             break
 
         sobrevivir += 1
-        print(f"dias sobrevividos: {sobrevivir}")
+        print(f"\nDias sobrevividos: {sobrevivir}\n")
 
     jugador = obtener_puntos(sobrevivir, jugador)
-    
+
     lista.append(jugador["nombre"])
     lista.append(jugador["puntos"])
 
